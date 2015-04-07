fca419ab4eaa0000fc18929f6dbe17d69b1461d0
root <root@li662-146.members.linode.com>
2015-04-07 07:27:19 +0000

    changes in css & enable metatag & redicrect module

--- sites/all/themes/md_leaders/templates/html.tpl.php
+++ sites/all/themes/md_leaders/templates/html.tpl.php
@@ -87,7 +87,8 @@
       },
       shorteners: {
         bitly: {}
-      }
+      },
+      title: "HCL Technologies Launches #aMileForHer. Run a mile & dedicate it to the women in your life. Visit: www.amileforher.in",
     }
     addthis.layers({
       'theme': 'transparent',
@@ -101,16 +102,5 @@
     });
   </script>
 
-  <script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-20585382-4']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
 </body>
 </html>
