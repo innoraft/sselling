be2fbca89b203158cf806f868e85b300fb7056c4
innoraft <aman.goel@innoraft.com>
2015-12-20 11:14:04 +0530

    Fix more submenu

--- sites/all/themes/md_leaders/js/home-page.js
+++ sites/all/themes/md_leaders/js/home-page.js
@@ -13,28 +13,34 @@
 
 			  // Make mobile banner image hyperlink.
 			  $('.block-inno-mobile-slider #slider1_container img').wrap($("<a/>").attr("href", "http://bit.ly/YouAreTheKeyBanner"));
+		  });
 
-			  // Make more link unclickable.
-			  $("#mainmenu li a:contains('More')").removeAttr('href');
+		  // Add more sub menu.
+		  $(document).ready(function() {
+			  if ($(window).width() > 991) {
+			  	var manu = '<li id="more-submenu"><a id="more-link">More</a><ul id="more-list"><li><a href="/social-selling-blogs">Social Selling Blogs</a></li><li><a href="/social-selling-case-studies">Social Selling Case Studies</a></li></ul></li>';
+			  	$("#mainmenu > li:nth-last-child(3)").after(manu);
+			  	$("#mainmenu > li:nth-last-child(1)").hide();
+			  	$("#mainmenu > li:nth-last-child(2)").hide();
+			  } else {
+			  	var manu = '<li id="more-submenu"><a id="more-link">More</a><ul id="more-list"><li><a href="/social-selling-blogs">Social Selling Blogs</a></li><li><a href="/social-selling-case-studies">Social Selling Case Studies</a></li></ul></li>';
+			  	$("#mainmenu > li:nth-last-child(3)").after(manu);
+			  	$("#mainmenu #more-submenu").hide();
+			  }
+		  });
 
-			  $("#mainmenu li a:contains('More')").css({
-			  	'color' : '#ffffff',
-			  	'cursor' : 'default',
-			  	'background-color' : '#006BB6'
-			  });
-
-			  // Align drop down of more link.
-			  $('#mainmenu .dropdown-menu').css({
-			  	'padding' : '8px',
-			  	'left' : 'auto',
-			  	'right' : '-12%'
-			  });
-
-			  // Attach drop down arrow to more link. 
-			  $("#mainmenu li a:contains('More')").closest('li').css({
-			  	'padding-right' : '8px',
-			  	'background' : "url('/sites/all/themes/md_leaders/img/drop-down.png') no-repeat 59px 24px / 21px auto"
-			  });
+		  $(window).load(function() {
+		  	$(window).resize(function() {
+		  		  if ($(window).width() > 991) {
+		  		  	$("#mainmenu #more-submenu").show();
+		  		  	$("#mainmenu > li:nth-last-child(1)").hide();
+		  		  	$("#mainmenu > li:nth-last-child(2)").hide();
+		  		  } else {
+		  		  	$("#mainmenu #more-submenu").hide();
+		  		  	$("#mainmenu > li:nth-last-child(1)").show();
+		  		  	$("#mainmenu > li:nth-last-child(2)").show();
+		  		  }
+		  	});
 		  });
 
 	    }
