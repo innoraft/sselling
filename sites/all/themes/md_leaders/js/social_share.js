0cafe87b7c69259e849581303e7dd7884626e23a
innoraft <aman.goel@innoraft.com>
2015-12-29 17:45:27 +0530

    Change social sharing text

--- sites/all/themes/md_leaders/js/social_share.js
+++ sites/all/themes/md_leaders/js/social_share.js
@@ -6,6 +6,10 @@
     });
     $('a.at-svc-twitter').live('mouseenter', function() {
       addthis_share.title = "Order Bloomsbury's #SocialSelling book #YouAreTheKey by @a1purva @gauravkakkar82 Order now at http://www.amazon.in/You-are-Key-Unlocking-Through/dp/9385436600/﻿";
+      addthis_share.url = "' '";
+    });
+    $('a.at-svc-twitter').live('mouseleave', function() {
+      addthis_share.url = "";
     });
     $('a.at-svc-google_plusone_share').live('mouseenter', function() {
       addthis_share.title = "Order  Bloomsbury's #SocialSelling book #YouAreTheKey by +Apurva Chamaria +Gaurav Kakkar at http://www.amazon.in/You-are-Key-Unlocking-Through/dp/9385436600/﻿";
