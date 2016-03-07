a0a5e588cc2a342c6f9cd8e13add57e2f45c93e8
jainaman23 <jainaman23@gmail.com>
2016-03-08 03:01:20 +0530

    added js

--- sites/all/themes/md_leaders/js/locker.js
+++ sites/all/themes/md_leaders/js/locker.js
@@ -4,15 +4,14 @@
 (function ($) {
 	Drupal.behaviors.combination_lock = {
 		attach: function (context, settings) {
-
-			$(".locker-upper").click(function(){
-				$(this).animate({top: "3px"}, 200, 'swing');
-			});
-
-			$(".locker-lower").click(function(){
+			var answer = $('#combination-lock-form .combination-lock-answer').text();
+			if (answer == 'TRUE') {
+				$(".locker-upper").animate({top: "3px"}, 200, 'swing');
+			}
+			else if (answer == 'FALSE') {
 				$(".locker-upper").animate({top: "45px"}, 200, 'swing').animate({top: "50px"}, 200, 'swing');
 				$(".locker-upper").animate({top: "45px"}, 200, 'swing').animate({top: "50px"}, 200, 'swing');
-			});
+			}
 		}
 	}
 })(jQuery)
