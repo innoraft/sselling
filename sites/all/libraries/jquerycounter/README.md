44083d76e79a80d7c93530814572f58a0eb96163
Akash Jain <akashjain132@gmail.com>
2016-03-08 21:54:21 +0530

    Add counter in quiz page

---
+++ sites/all/libraries/jquerycounter/README.md
@@ -0,0 +1,13 @@
+jQuery Counter Plus One
+=======================
+
+jQuery plugin that animates a counter for any purpose like page views,
+newsletter subscribers, time counter, etc.
+
+Check the file example.html to see how to implement it, it has a simple example
+plus the html markup needed and the plugin options.
+
+Download the latest version: [jquery-counter-plus-one zip file](https://dl.dropboxusercontent.com/u/26835042/github/jquery-counter-plus-one.zip).
+
+------------------------
+Released under MIT License.
--- sites/all/themes/md_leaders/js/locker.js
+++ sites/all/themes/md_leaders/js/locker.js
@@ -12,6 +12,46 @@
 				$(".locker-upper").animate({top: "45px"}, 200, 'swing').animate({top: "50px"}, 200, 'swing');
 				$(".locker-upper").animate({top: "45px"}, 200, 'swing').animate({top: "50px"}, 200, 'swing');
 			}
+
+			var answer = $('#combination-lock-form input[name = lock_pattern]').val();
+
+			if (answer.length < 2) {
+				for (var i = answer.length - 1; i >= 0; i--) {
+					answer = '0' + answer;
+        }
+			}
+
+			var initial_counter = 0;
+	    $('.lock-pattern-first-counter').counter({
+	      val: initial_counter,
+	      countTrigger: function ($counter) {
+	        setInterval(function() {
+						var lastNum = answer[answer.length - 2];
+	          var n_counter = $counter.text().replace(/\D/g,"");
+	        	if (lastNum >= 0 && n_counter != lastNum) {
+              $.counter.plusOne($counter);
+            }
+	        }, 500);
+	      }
+	    });
+
+	    $('.lock-pattern-second-counter').counter({
+	      val: initial_counter,
+	      countTrigger: function ($counter) {
+	        setInterval(function() {
+						var lastNum = answer[answer.length - 1];
+	          var n_counter = $counter.text().replace(/\D/g,"");
+        		console.log("====");
+        		console.log(lastNum);
+        		console.log(n_counter);
+        		console.log("====");
+	        	if (lastNum >= 0 && n_counter != lastNum) {
+              $.counter.plusOne($counter);
+            }
+	        }, 500);
+	      }
+	    });
+
 		}
 	}
 })(jQuery)
